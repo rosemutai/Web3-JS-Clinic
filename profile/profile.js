4f8cb32acccb5ff5f80a4eb78d85a527f0a1b3d6
rosemutai <chepngetichrose2030@gmail.com>
2022-09-13 10:16:02 +0300

    profile card information

---
+++ profile/profile.js
@@ -0,0 +1,11 @@
+const myCareer = document.getElementById('career')
+
+function changeBgColor () {
+    myCareer.style.boxShadow = '8px 4px 22px #15133C';
+    myCareer.style.transform = "scale(1.2)"
+     myCareer.style.marginBlock = "1.8rem"
+    myCareer.style.transition = "all 2s"
+
+}
+
+
